bb23248ec5e46caad5416bb6bd67707e4b3d8aae
Knabe, Christoph <knabe@beuth-hochschule.de>
2017-10-26 17:40:08 +0200

    Spacing

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,17 +1,17 @@
 import sbt._
 
 object Version {
-  val scala     = "2.12.3" //was "2.11.11"
-  val akkaHttp  = "10.0.9" //was "0.11"
-  val akka      = "2.4.19" //was "2.3.7"
-  val logback   = "1.1.2"
+  val scala = "2.12.3" //was "2.11.11"
+  val akkaHttp = "10.0.9" //was "0.11"
+  val akka = "2.4.19" //was "2.3.7"
+  val logback = "1.1.2"
   val scalaTest = "3.0.3" //was "2.2.2"
 }
 
 object Library {
-  val akkaHttp       = "com.typesafe.akka" %% "akka-http-core" % Version.akkaHttp
-  val akkaSlf4j      = "com.typesafe.akka" %% "akka-slf4j"             % Version.akka
-  val akkaTestkit    = "com.typesafe.akka" %% "akka-testkit"           % Version.akka
-  val logbackClassic = "ch.qos.logback"    %  "logback-classic"        % Version.logback
-  val scalaTest      = "org.scalatest"     %% "scalatest"              % Version.scalaTest
+  val akkaHttp = "com.typesafe.akka" %% "akka-http-core" % Version.akkaHttp
+  val akkaSlf4j = "com.typesafe.akka" %% "akka-slf4j" % Version.akka
+  val akkaTestkit = "com.typesafe.akka" %% "akka-testkit" % Version.akka
+  val logbackClassic = "ch.qos.logback" % "logback-classic" % Version.logback
+  val scalaTest = "org.scalatest" %% "scalatest" % Version.scalaTest
 }
